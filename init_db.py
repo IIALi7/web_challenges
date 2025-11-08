d34500ca71161b47fe0e8217d66cac294006c642
IIALi7 <0kv.s777@gmail.com>
2025-11-08 14:38:12 -0500

    CTF: add flags, simulated reverse shell, challenges routes, index

--- init_db.py
+++ init_db.py
@@ -9,7 +9,6 @@ else:
     cur.execute("CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT, password TEXT);")
     cur.executemany("INSERT INTO users(id, username, password) VALUES (?, ?, ?);",
                     [(1,'admin','admin123'), (2,'guest','guest')])
-    # create flags table for SQLi challenge
     cur.execute("CREATE TABLE flags(id INTEGER PRIMARY KEY, flag TEXT);")
     cur.execute("INSERT INTO flags(flag) VALUES (?)", ("CSC{SQLI_FLAG_2}",))
     conn.commit()
