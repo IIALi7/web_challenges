7db62632b7ecd8cd637454785f4c13452f7c61a1
IIALi7 <0kv.s777@gmail.com>
2025-11-08 14:18:47 -0500

    Fix: bind Flask to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -113,4 +113,7 @@ def hidden(name):
     abort(404)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)), debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
